15d2699dbcdc439262f215ce6249703830bf5332
Simone Taeggi <startae14@gmail.com>
2020-11-20 19:36:50 +0100

    create the mongo schema of our Models

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const mongoose = require("mongoose");
 require("dotenv").config({
   path: "variables.env",
 });
+const User = require("./models/User");
+const Post = require("./models/Post");
 
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
   })
   .then(() => console.log("DB CONNECTED"))
   .catch((err) => console.error(err));
@@ -26,6 +29,10 @@ const typeDefs = gql`
 // initialize server
 const server = new ApolloServer({
   typeDefs,
+  context: {
+    User,
+    Post,
+  },
 });
 
 server.listen().then(({ url }) => {
