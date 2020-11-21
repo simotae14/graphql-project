90a271785a1b8bc59c6c235b5cb40b70b78de7c4
Simone <startae14@gmail.com>
2020-11-21 12:40:46 +0100

    create the side navbar with links and effects on click

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,38 @@
 <template>
-  <v-app>
-    <!-- App Title -->
+  <v-app style="background: #E3E3EE">
+    <!-- Side Navbar -->
+    <v-navigation-drawer app temporary fixed v-model="sideNav">
+      <v-toolbar color="accent" dark flat>
+        <v-app-bar-nav-icon @click="toggleSideNav"></v-app-bar-nav-icon>
+        <router-link style="cursor: pointer" to="/" tag="span">
+          <h1 class="title pl-3">
+            VueTaeShare
+          </h1>
+        </router-link>
+      </v-toolbar>
+      <v-divider></v-divider>
+      <!-- Side Navbar Links -->
+      <v-list>
+        <v-list-item
+          v-for="item in sideNavItems"
+          :key="item.title"
+          :to="item.link"
+          ripple
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            {{ item.title }}
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
     <!-- Horizontal Navbar -->
     <v-app-bar fixed dark color="primary">
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click="toggleSideNav"></v-app-bar-nav-icon>
       <v-toolbar-title class="hidden-xs-only">
+        <!-- App Title -->
         <router-link style="cursor: pointer" to="/" tag="span"
           >VueTaeShare</router-link
         >
@@ -39,7 +67,6 @@
       <v-container class="mt-4">
         <router-view />
       </v-container>
-      <router-view />
     </main>
   </v-app>
 </template>
@@ -47,6 +74,11 @@
 <script>
 export default {
   name: "App",
+  data() {
+    return {
+      sideNav: false,
+    };
+  },
   computed: {
     horizontalNavItems() {
       return [
@@ -66,6 +98,30 @@ export default {
           link: "/signup",
         },
       ];
+    },
+    sideNavItems() {
+      return [
+        {
+          icon: "chat",
+          title: "Posts",
+          link: "/posts",
+        },
+        {
+          icon: "lock_open",
+          title: "Sign In",
+          link: "/signin",
+        },
+        {
+          icon: "create",
+          title: "Sign Up",
+          link: "/signup",
+        },
+      ];
+    },
+  },
+  methods: {
+    toggleSideNav() {
+      this.sideNav = !this.sideNav;
     },
   },
 };
