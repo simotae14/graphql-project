18660cb893a39033444952ef152384efd070c9ab
Simone <startae14@gmail.com>
2020-11-21 16:50:53 +0100

    add first query inside a component using apollo

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,10 +1,37 @@
 <template>
-  <h1>Home</h1>
+  <v-container>
+    <h1>Home</h1>
+    <ul v-for="post in getPosts" :key="post._id">
+      <li>
+        {{ post.title }}
+        {{ post.imageUrl }}
+        {{ post.description }}
+      </li>
+      <li>{{ post.likes }}</li>
+    </ul>
+  </v-container>
 </template>
 
 <script>
+import { gql } from "apollo-boost";
+
 // @ is an alias to /src
 export default {
   name: "home",
+  apollo: {
+    getPosts: {
+      query: gql`
+        query {
+          getPosts {
+            _id
+            title
+            imageUrl
+            description
+            likes
+          }
+        }
+      `,
+    },
+  },
 };
 </script>
